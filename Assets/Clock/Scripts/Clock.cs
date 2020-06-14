afafd78ad12d324c5931d373b27f8664ffd70114
Vincent Scavetta <vstro24@gmail.com>
2020-06-14 16:06:22 -0400

    Adding the unity project

---
+++ Assets/Clock/Scripts/Clock.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+
+    const float 
+        degreesPerHour = 30f,
+        degreesPerMinute = 6f,
+        degreesPerSecond = 6f;
+
+    public bool isContinuous;
+
+    public Transform hoursTransform, minutesTransform, secondsTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isContinuous)
+        {
+            UpdateContinuous();
+        } 
+        else
+        {
+            UpdateDiscrete();
+        }
+    }
+
+    void Awake()
+    {
+        var now = DateTime.Now;
+        Debug.Log(now);
+        hoursTransform.localRotation = Quaternion.Euler(0f, now.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, now.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, now.Second * degreesPerSecond, 0f);
+    }
+
+    void UpdateContinuous()
+    {
+        var now = DateTime.Now.TimeOfDay;
+        hoursTransform.localRotation = Quaternion.Euler(0f, (float) now.TotalHours * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, (float) now.TotalMinutes * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, (float) now.TotalSeconds * degreesPerSecond, 0f);
+    }
+
+    void UpdateDiscrete()
+    {
+        var now = DateTime.Now;
+        hoursTransform.localRotation = Quaternion.Euler(0f, now.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, now.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, now.Second * degreesPerSecond, 0f);
+    }
+}
---
+++ Assets/Graph/Graph.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+
+    [Range(10, 100)]
+    public int resolution = 10;
+    public Transform pointPrefab;
+    Transform[] points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            point.localPosition = position;
+        }
+    }
+
+    void Awake()
+    {
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
+        Vector3 position = new Vector3
+        {
+            x = 0f
+        };
+        //position.y = 0f;
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            position.x = ((i + 0.5f) * step - 1f);
+            //position.y = position.x * position.x * position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(this.transform, false);
+            points[i] = point;
+        }
+    }
+}
